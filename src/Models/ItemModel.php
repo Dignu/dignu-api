c32359a8cd6e652c4f2037cb631a9a0586dc41e6
Rafael Aguiar <rafael.henrique.aguiar@outlook.com.br>
2023-08-06 20:46:55 -0300

    chore: item model finished

--- src/Models/ItemModel.php
+++ src/Models/ItemModel.php
@@ -5,10 +5,33 @@ namespace Src\Models;
 
 class ItemModel
 {
+
     public function __construct(
+        private int $id,
+        private int $unitId,
+        private string $title,
+        private string $description,
+        private float $price,
     ) {
     }
 
+public function getId() : int {
+    return $this->id;
+}
 
+public function getUnitId() : int {
+    return $this->unitId;
+}
+
+public function getTitle() : string {
+    return $this->title;
+}
+public function getDescription() : string {
+    return $this->description;
+}
+
+public function getPrice() : float {
+    return $this->price;
+}
 
 }
